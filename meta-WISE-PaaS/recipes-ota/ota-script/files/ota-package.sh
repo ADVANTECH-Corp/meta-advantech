4b4793f59848f5df7fe7a05f6a5692ce1393492d
Daniel Hung <daniel.hung@advantech.com.tw>
2017-03-31 08:10:04 +0000

    [OTA] Support DTB image for OTA package

--- meta-WISE-PaaS/recipes-ota/ota-script/files/ota-package.sh
+++ meta-WISE-PaaS/recipes-ota/ota-script/files/ota-package.sh
@@ -10,6 +10,7 @@ Generate package file to be used for OTA update on Yocto Linux
 
  -b <file>  Bootloader image file
  -k <file>  Kernel or Boot image file
+ -d <file>  DTB (device tree) file
  -r <file>  Rootfs image file
  -o <file>  Output file
  -h         Show help
@@ -19,13 +20,16 @@ EOF
 
 OUTPUT="update"
 
-while getopts "b:k:r:o:h" o; do
+while getopts "b:k:d:r:o:h" o; do
     case "${o}" in
     b)
         BOOTLOADER=${OPTARG}
         ;;
     k)
         KERNEL=${OPTARG}
+        ;;
+    d)
+        DTB=${OPTARG}
         ;;
     r)
         ROOTFS=${OPTARG}
@@ -39,7 +43,7 @@ while getopts "b:k:r:o:h" o; do
     esac
 done
 
-if [ -z ${BOOTLOADER} ] && [ -z ${KERNEL} ] && [ -z ${ROOTFS} ] ; then
+if [ -z ${BOOTLOADER} ] && [ -z ${KERNEL} ] && [ -z ${DTB} ] && [ -z ${ROOTFS} ] ; then
     echo "Please specify at least one image file"
     echo ""
     usage
@@ -48,6 +52,7 @@ fi
 
 CMD_BOOTLOADER="update-bootloader"
 CMD_KERNEL="update-kernel"
+CMD_DTB="update-dtb"
 CMD_ROOTFS="update-rootfs"
 
 UPDATE_DIR="ota-update"
@@ -59,17 +64,22 @@ mkdir ${UPDATE_DIR}
 if [ ! -z ${BOOTLOADER} ] ; then
     cp ${BOOTLOADER} ${UPDATE_DIR}
     MD5_SUM=`md5sum -b ${BOOTLOADER} | cut -d ' ' -f 1`
-    echo "${CMD_BOOTLOADER}, ${BOOTLOADER}, ${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
+    echo "${CMD_BOOTLOADER},${BOOTLOADER},${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
 fi
 if [ ! -z ${KERNEL} ] ; then
     cp ${KERNEL} ${UPDATE_DIR}
     MD5_SUM=`md5sum -b ${KERNEL} | cut -d ' ' -f 1`
-    echo "${CMD_KERNEL}, ${KERNEL}, ${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
+    echo "${CMD_KERNEL},${KERNEL},${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
+fi
+if [ ! -z ${DTB} ] ; then
+    cp ${DTB} ${UPDATE_DIR}
+    MD5_SUM=`md5sum -b ${DTB} | cut -d ' ' -f 1`
+    echo "${CMD_DTB},${DTB},${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
 fi
 if [ ! -z ${ROOTFS} ] ; then
     cp ${ROOTFS} ${UPDATE_DIR}
     MD5_SUM=`md5sum -b ${ROOTFS} | cut -d ' ' -f 1`
-    echo "${CMD_ROOTFS}, ${ROOTFS}, ${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
+    echo "${CMD_ROOTFS},${ROOTFS},${MD5_SUM}" >> ${UPDATE_DIR}/${UPDATE_SCRIPT}
 fi
 
 zip -r ${OUTPUT} ${UPDATE_DIR}/*
