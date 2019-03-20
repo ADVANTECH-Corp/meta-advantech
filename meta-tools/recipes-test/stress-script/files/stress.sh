9739051cf2e7147e78ead8981ef324462dd21abc
Daniel Hung <daniel.hung@advantech.com.tw>
2019-03-20 10:53:22 +0000

    Update stress script
    Apply the same stress script for both i.MX6 & APQ8016.
    To turn on/off service for SysV system, use the commands below.
      $ update-rc.d power-test defaults
      $ update-rc.d -f power-test remove

--- meta-tools/recipes-test/stress-script/files/stress.sh
+++ meta-tools/recipes-test/stress-script/files/stress.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
+THERMAL_ZONE0_TEMP="/sys/devices/virtual/thermal/thermal_zone0/temp"
+THERMAL_ZONE1_TEMP="/sys/devices/virtual/thermal/thermal_zone1/temp"
+COOLING_DEVICE0_STATE="/sys/class/thermal/cooling_device0/cur_state"
+COOLING_DEVICE1_STATE="/sys/class/thermal/cooling_device1/cur_state"
+CPU_FREQENCY="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq"
+
+echo 0 > /sys/devices/virtual/thermal/thermal_zone0/trip_point_0_temp
+echo 0 > /sys/devices/virtual/thermal/thermal_zone1/trip_point_0_temp
 echo "disabled" > /sys/devices/virtual/thermal/thermal_zone0/mode
 echo "disabled" > /sys/devices/virtual/thermal/thermal_zone1/mode
-echo "performance" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-xterm -geometry 80x4+10+10 -e watch -n 1 cat /sys/class/thermal/thermal_zone0/temp &
-xterm -geometry 80x4+10+110 -e watch -n 1 cat /sys/class/thermal/thermal_zone1/temp &
-xterm -geometry 80x4+10+210 -e watch -n 1 cat /sys/class/thermal/cooling_device0/cur_state &
-xterm -geometry 80x4+10+310 -e watch -n 1 cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_cur_freq &
-xterm -geometry 80x40+10+410 top &
+[ -e ${THERMAL_ZONE0_TEMP} ] && \
+xterm -title thermal_zone0 -geometry 80x4+10+10 -e watch -n 1 cat ${THERMAL_ZONE0_TEMP} &
 
-stress -c 4 -i 4 -d 1 --hdd-bytes 512
+[ -e ${THERMAL_ZONE1_TEMP} ] && \
+xterm -title thermal_zone1 -geometry 80x4+10+110 -e watch -n 1 cat ${THERMAL_ZONE1_TEMP} &
+
+[ -e ${COOLING_DEVICE0_STATE} ] && \
+xterm -title cooling_device0 -geometry 80x4+10+210 -e watch -n 1 cat ${COOLING_DEVICE0_STATE} &
+
+[ -e ${COOLING_DEVICE1_STATE} ] && \
+xterm -title cooling_device1 -geometry 80x4+10+310 -e watch -n 1 cat ${COOLING_DEVICE1_STATE} &
+
+[ -e ${CPU_FREQENCY} ] && \
+xterm -title cpu_freq -geometry 80x4+10+410 -e watch -n 1 cat ${CPU_FREQENCY} &
+
+xterm -title top -geometry 80x40+10+510 top &
+
+stress -c 4
 #glmark2 --size 1280x720 --annotate --run-forever &
--- meta-tools/recipes-test/stress-script/stress-script.bb
+++ meta-tools/recipes-test/stress-script/stress-script.bb
@@ -3,6 +3,7 @@ LICENSE = "GPL-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=801f80980d171dd6425610833a22dbe6"
 
 SRC_URI = "file://stress.sh \
+	   file://power-test \
 	   file://power-test.service"
 
 inherit systemd
@@ -11,11 +12,20 @@ do_install() {
     install -d ${D}/tools
     install -m 755 ${WORKDIR}/stress.sh ${D}/tools/stress.sh
 
-    install -d ${D}${systemd_unitdir}/system
-    install -m 0644 ${WORKDIR}/power-test.service ${D}${systemd_unitdir}/system
+    # SysV
+    if ${@bb.utils.contains('DISTRO_FEATURES','sysvinit','true','false',d)}; then
+        install -d ${D}${sysconfdir}/init.d
+        install -m 0755 ${WORKDIR}/power-test ${D}${sysconfdir}/init.d/power-test
+    fi
+
+    # Systemd
+    if ${@bb.utils.contains('DISTRO_FEATURES','systemd','true','false',d)}; then
+        install -d ${D}${systemd_unitdir}/system
+        install -m 0644 ${WORKDIR}/power-test.service ${D}${systemd_unitdir}/system
+    fi
 }
 
 SYSTEMD_SERVICE_${PN} = "power-test.service"
 SYSTEMD_AUTO_ENABLE_${PN} = "disable"
 
-FILES_${PN} = "/tools"
+FILES_${PN} = "/tools ${sysconfdir}/init.d"
