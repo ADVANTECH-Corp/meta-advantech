98f510edc62bd9358a95896085843f33879dc627
Daniel Hung <daniel.hung@advantech.com.tw>
2019-08-29 07:36:52 +0000

    [OTA] Do not need to remove ZIP file for new OTA handler

--- meta-WISE-PaaS/recipes-ota/ota-script/files/do_update.sh
+++ meta-WISE-PaaS/recipes-ota/ota-script/files/do_update.sh
@@ -51,17 +51,6 @@ if [ -e $FILE_PATH ] ; then
     mkdir -p /cache/recovery/
     echo "--update_package=${FILE_PATH}" > /cache/recovery/command
 
-    echo "Remove ZIP package ..."
-    FILE_BASE_NAME=`expr ${FILE_PATH} : '.*[/]\(.*-v[0-9].[0-9].[0-9].[0-9]-.*\)'`
-    ZIP_FILE_NAME=`echo ${FILE_BASE_NAME} | cut -d '/' -f 1`
-    ZIP_FILE_PATH=`find /cache/ -name ${ZIP_FILE_NAME}.zip`
-    echo "rm ${ZIP_FILE_PATH}"
-    rm ${ZIP_FILE_PATH}
-    ZIP_2ND_FILE_PATH=${ZIP_FILE_PATH%.zip}
-    ZIP_2ND_FILE_NAME=`grep -r PackageType ${ZIP_2ND_FILE_PATH}/PackageInfo.xml | cut -d '>' -f 2 | cut -d '<' -f 1`
-    echo "rm ${ZIP_2ND_FILE_PATH}/${ZIP_2ND_FILE_NAME}.zip"
-    rm ${ZIP_2ND_FILE_PATH}/${ZIP_2ND_FILE_NAME}.zip
-
     echo "Write BCB ..."
     echo -ne "\x62\x6f\x6f\x74\x2d\x72\x65\x63\x6f\x76\x65\x72\x79\x00" > /cache/boot-recovery
     dd if=/cache/boot-recovery of=${DISK_DIR}/misc
